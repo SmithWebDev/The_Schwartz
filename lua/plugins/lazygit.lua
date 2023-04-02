88190a029a1aabd4b6625796b754372dffc7bc1d
Darrick Smith <smithwebdd@gmail.com>
2023-04-02 14:48:08 -0400

    Added Lazygit

---
+++ lua/plugins/lazygit.lua
@@ -0,0 +1,5 @@
+return {
+  "kdheepak/lazygit.nvim", -- https://github.com/kdheepak/lazygit.nvim
+  event = "VeryLazy",
+  keys = { { "<leader>gl", "<cmd>LazyGit<CR>", desc = "Lazygit toggle" } },
+}
