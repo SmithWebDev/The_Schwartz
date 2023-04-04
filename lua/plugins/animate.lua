9936fd4caff7d2f795480906df7b78f793b040d0
Darrick Smith <smithwebdd@gmail.com>
2023-04-04 00:42:21 -0400

    Added Animate and Lens

---
+++ lua/plugins/animate.lua
@@ -0,0 +1,5 @@
+  return {
+    'camspiers/animate.vim',                          -- https://github.com/camspiers/animate.vim
+    dependencies = 'camspiers/lens.vim',              -- https://github.com/camspiers/lens.vim
+  }
+
