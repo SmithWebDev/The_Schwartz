8601ff81f6136738d143e1834c797dcb5dd3c9e3
Darrick Smith <smithwebdd@gmail.com>
2023-04-04 10:48:53 -0400

    Added Emmet-Vim

---
+++ lua/plugins/emmet.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    'mattn/emmet-vim', -- https://github.com/mattn/emmet-vim
+    init = function()
+      vim.g.user_emmet_leader_key = '<leader>,'
+      vim.g.user_emmet_settings = {
+        javascript = {
+          attribute = {
+            { ['for'] = 'htmlFor' },
+            { ['class'] = 'className' }
+          },
+          javascript = { extends = 'jsx' },
+          typescript = { extends = 'tsx' }
+          -- ['erb'] = {
+          --   {extends = 'html'}
+          --   },
+        }
+      }
+      vim.g.user_emmet_mode = 'inv'
+    end
+  },
+}
