d209afcc3c8b424b321188a1ee555d9d97bd811e
Darrick Smith <smithwebdd@gmail.com>
2023-04-04 02:17:37 -0400

    Added Snippet Genie

---
+++ lua/plugins/snippet_genie.lua
@@ -0,0 +1,6 @@
+return {
+  'ziontee113/SnippetGenie',                        -- https://github.com/ziontee113/SnippetGenie
+  opts = {
+    snippets_directory = '~/.config/nvim/lua/snippets/'
+  }
+}
