d61f0974c18a7058851ee54d3062c700e4c540b1
Darrick Smith <smithwebdd@gmail.com>
2023-04-04 00:52:52 -0400

    Updated NeoTree config

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -2,13 +2,15 @@ return {
   'nvim-neo-tree/neo-tree.nvim',                    -- https://github.com/nvim-neo-tree/neo-tree.nvim
   opts = {
     close_if_last_window = true,
-    window = {
-      width = 30,
-      mappings = {
-      ["-"] = "open_split",
-      ["\\"] = "open_vsplit",
-      }
-    }
+    filesystem = {
+      window = {
+        width = 30,
+        mappings = {
+          ["-"] = "open_split",
+          ["\\"] = "open_vsplit",
+        },
+      },
+    },
   },
 }
 
