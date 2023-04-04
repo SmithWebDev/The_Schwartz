21a1f6aa1bd2ebc9e23a7d27d1f6e0e2268d85f8
Darrick Smith <smithwebdd@gmail.com>
2023-04-04 10:04:37 -0400

    Updated Neotree with nvim window picker plugin

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,16 +1,46 @@
 return {
-  'nvim-neo-tree/neo-tree.nvim',                    -- https://github.com/nvim-neo-tree/neo-tree.nvim
-  opts = {
-    close_if_last_window = true,
-    filesystem = {
-      window = {
-        width = 30,
-        mappings = {
-          ["-"] = "open_split",
-          ["\\"] = "open_vsplit",
+  {
+    'nvim-neo-tree/neo-tree.nvim',                    -- https://github.com/nvim-neo-tree/neo-tree.nvim
+    opts = {
+      close_if_last_window = true,
+      filesystem = {
+        window = {
+          width = 30,
+          mappings = {
+            ["-"] = "open_split",
+            ["\\"] = "open_vsplit",
+          },
         },
       },
     },
   },
-}
+  {
+    's1n7ax/nvim-window-picker',                  -- https://github.com/s1n7ax/nvim-window-picker
+    event = 'VeryLazy',
+    version = '1.*',
+    opts = {
+      autoselect_one = true,
+      include_current_win = false,
+      filter_rules = {
+        -- filter using buffer options
+        bo = {
+          -- if the file type is one of following, the window will be ignored
+          filetype = { 'neo-tree', "neo-tree-popup", "notify" },
 
+          -- if the buffer type is one of following, the window will be ignored
+          buftype = { 'terminal', "quickfix" },
+        },
+      },
+      other_win_hl_color = '#e35e4f',
+    },
+    keys ={
+      {
+        '<leader>/',
+        function()
+          local picked_window_id = require('window-picker').pick_window() or vim.api.nvim_get_current_win()
+          vim.api.nvim_set_current_win(picked_window_id)
+        end,  desc = 'Pick a Window'
+      },
+    }
+  }
+}
