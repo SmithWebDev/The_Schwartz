63eefab5385ba232c826a47419233fe9323606e0
Darrick Smith <smithwebdd@gmail.com>
2023-04-04 10:05:27 -0400

    Added Vim-Smoothie to Animate file

--- lua/plugins/animate.lua
+++ lua/plugins/animate.lua
@@ -1,5 +1,8 @@
-  return {
-    'camspiers/animate.vim',                          -- https://github.com/camspiers/animate.vim
-    dependencies = 'camspiers/lens.vim',              -- https://github.com/camspiers/lens.vim
-  }
+return {
+  'camspiers/animate.vim',                          -- https://github.com/camspiers/animate.vim
+  dependencies = {
+    'camspiers/lens.vim',              -- https://github.com/camspiers/lens.vim
+    'psliwka/vim-smoothie',                             -- https://github.com/psliwka/vim-smoothie
+  },
+}
 
