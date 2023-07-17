23b21e1efa90e0bbaa46465f2eee257b9248517c
Darrick Smith <smithwebdd@gmail.com>
2023-07-17 13:07:49 -0400

    Updated lua Snippet

--- lua/snippets/lua.lua
+++ lua/snippets/lua.lua
@@ -1,6 +1,11 @@
 return {
-  s('snip',
-    c(1,{fmt([[
+  s(
+    "snip",
+    c(
+      1,
+      {
+        fmt(
+          [[
     -- {}
     s({{
       trig = '{}',
@@ -10,20 +15,39 @@ return {
       {}
     ),
 
-    ]], {
-        i(1),
-        i(2),
-        i(3),
-        rep(1),
-        c(4,{
-          {t('{'),t(""),
-            t('\t'),i(1),t(''),
-            t('}')},
-          {t('fmt([['), i(1), t(']], {'), i(2), t('})')}
-        }),
-      }),
-    }
+    ]],
+          {
+            i(1),
+            i(2),
+            i(3),
+            rep(1),
+            c(4, {
+              { t("{"), t(""), t("\t"), i(1), t(""), t("}") },
+              { t("fmt([["), i(1), t("]], {"), i(2), t("})") },
+            }),
+          }
+        ),
+      }
     )
   ),
-
+  -- Neovim Keybind skeleton
+  s(
+    {
+      trig = "key",
+      name = "Keybind Skeleton",
+      dscr = "Neovim Keybind skeleton",
+    },
+    fmt([[vim.keymap.set('{}', '{}', '{}', {{ desc = '{}', silent = true }})]], {
+      c(1, {
+        t("n"),
+        t("i"),
+        t("v"),
+        t("t"),
+        t("x"),
+      }),
+      i(2),
+      i(3),
+      i(4),
+    })
+  ),
 }
